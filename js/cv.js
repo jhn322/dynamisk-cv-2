062c7671acbe612d17acb233323d672444294091
Johan Söderlund <johan.soderlund96@gmail.com>
2023-11-16 19:24:24 +0100

    changed order of title

--- js/cv.js
+++ js/cv.js
@@ -7,30 +7,30 @@
 
 // Retrieving Id from cv.html for each job and education
 const job1Year = document.getElementById("job1-year");
-const job1Company = document.getElementById("job1-company");
 const job1Title = document.getElementById("job1-title");
+const job1Company = document.getElementById("job1-company");
 const job1Description = document.getElementById("job1-description");
 
 const job2Year = document.getElementById("job2-year");
-const job2Company = document.getElementById("job2-company");
 const job2Title = document.getElementById("job2-title");
+const job2Company = document.getElementById("job2-company");
 const job2Description = document.getElementById("job2-description");
 
 const education1Year = document.getElementById("education1-year");
-const education1School = document.getElementById("education1-school");
 const education1Title = document.getElementById("education1-title");
+const education1School = document.getElementById("education1-school");
 
 const education2Year = document.getElementById("education2-year");
-const education2School = document.getElementById("education2-school");
 const education2Title = document.getElementById("education2-title");
+const education2School = document.getElementById("education2-school");
 
 const education3Year = document.getElementById("education3-year");
-const education3School = document.getElementById("education3-school");
 const education3Title = document.getElementById("education3-title");
+const education3School = document.getElementById("education3-school");
 
 const education4Year = document.getElementById("education4-year");
-const education4School = document.getElementById("education4-school");
 const education4Title = document.getElementById("education4-title");
+const education4School = document.getElementById("education4-school");
 
 // A function to fetch and display the CV info from JSON file.
 async function getCV() {
