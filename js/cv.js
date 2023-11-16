3399600b9a050c0bce62a92f7df64b264bb1eb5c
Johan Söderlund <johan.soderlund96@gmail.com>
2023-11-16 01:24:40 +0100

    wrote down goal of JS file and created variables and retrieving id from html file.

---
+++ js/cv.js
@@ -0,0 +1,34 @@
+// The goal of this JavaScript file:
+// 1) Retrieve elements for 1st and 2nd job from HTML, year, company, title, description.
+// 2) Fetch the work experience from the JSON file which includes all properties (title, year etc).
+// 3) Retrieve elements for 1st, 2nd, 3rd and 4th education from HTML, year, school, title.
+// 4) Fetch the school experience from the JSON file which includes all properties (education, year etc).
+// 8) Display JSON CV info on the specific tab of the webpage.
+
+// Retrieving Id from cv.html for each job and education
+const job1Year = document.getElementById("job1-year");
+const job1Company = document.getElementById("job1-company");
+const job1Title = document.getElementById("job1-title");
+const job1Description = document.getElementById("job1-description");
+
+const job2Year = document.getElementById("job2-year");
+const job2Company = document.getElementById("job2-company");
+const job2Title = document.getElementById("job2-title");
+const job2Description = document
+
+const education1Year = document.getElementById("education1-year");
+const education1School = document.getElementById("education1-school");
+const education1Title = document.getElementById("education1-title");
+
+const education2Year = document.getElementById("education2-year");
+const education2School = document.getElementById("education2-school");
+const education2Title = document.getElementById("education2-title");
+
+const education3Year = document.getElementById("education3-year");
+const education3School = document.getElementById("education3-school");
+const education3Title = document.getElementById("education3-title");
+
+const education4Year = document.getElementById("education4-year");
+const education4School = document.getElementById("education4-school");
+const education4Title = document.getElementById("education4-title");
+
