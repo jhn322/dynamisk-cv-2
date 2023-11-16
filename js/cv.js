004d9273eda956773a48ac070658575a67827c45
Johan Söderlund <johan.soderlund96@gmail.com>
2023-11-16 01:41:14 +0100

    assigned variables to all HTML elemnts for job & education, logged error if failed and started the function. Applicable comments

--- js/cv.js
+++ js/cv.js
@@ -32,3 +32,47 @@ const education4Year = document.getElementById("education4-year");
 const education4School = document.getElementById("education4-school");
 const education4Title = document.getElementById("education4-title");
 
+// A function to fetch and display the CV info from JSON file.
+async function getCV() {
+    const response = await fetch("../json/cv.json");
+    if (response.ok) {
+        const json = await response.json();
+
+        // Assigning a variable to the corresponding HTML elements for each category and for each job and education & first array begins.
+        job1Year.textContent = json.workExperience[0].year;
+        job1Company.textContent = json.workExperience[0].company;
+        job1Title.textContent = json.workExperience[0].title;
+        job1Description.textContent = json.workExperience[0].description;
+
+        // Second object in the first array.
+        job2Year.textContent = json.workExperience[1].year;
+        job2Company.textContent = json.workExperience[1].company;
+        job2Title.textContent = json.workExperience[1].title;
+        job2Description.textContent = json.workExperience[1].description;
+
+        // Second array begins.
+        education1Year.textContent = json.schoolExperience[0].year;
+        education1School.textContent = json.schoolExperience[0].school;
+        education1Title.textContent = json.schoolExperience[0].education;
+
+        education2Year.textContent = json.schoolExperience[1].year;
+        education2School.textContent = json.schoolExperience[1].school;
+        education2Title.textContent = json.schoolExperience[1].education;
+
+        education3Year.textContent = json.schoolExperience[2].year;
+        education3School.textContent = json.schoolExperience[2].school;
+        education3Title.textContent = json.schoolExperience[2].education;
+
+        // Last items in the second array.
+        education4Year.textContent = json.schoolExperience[3].year;
+        education4School.textContent = json.schoolExperience[3].school;
+        education4Title.textContent = json.schoolExperience[3].education;
+
+        // Logging error if fetch request failed.
+    } else {
+        console.log("HTTP-error: " + response.status);
+    }
+}
+
+// Starting the function
+getCV();
