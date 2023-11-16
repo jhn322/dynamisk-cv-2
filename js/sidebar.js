f75c67c498a1f7d3f8e13568cf6e884669335e4a
Johan Söderlund <johan.soderlund96@gmail.com>
2023-11-17 00:22:00 +0100

    added open and close on hover, some delay for open

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,4 +1,31 @@
+// Functions of the sidebar: 
+// 1) Expand menu when hamburger button is pressed
+// 2) Contract when hamburger button is pressed
+// 3) Open with a delay when mouse hover the sidebar
+// 4) Close without a delay when mouse leaves the sidebar
+
 // Opens sidebar
 function toggleSidebar() {
   document.body.classList.toggle("open");
 }
+
+// Opens sidebar on mouse hover
+const sidebar = document.querySelector('.sidebar');
+
+// Variable for delayed opening of sidebar on mouse hover
+let timeout;
+
+// Function to open sidebar
+sidebar.addEventListener('mouseenter', function() {
+  timeout = setTimeout(function() {
+    document.body.classList.add('open');
+  }, 500); // 500ms delay
+});
+
+// Function to close sidebar
+sidebar.addEventListener('mouseleave', function() {
+document.body.classList.remove('open');
+
+// Cancels the timeout if the mouse leaves before 500ms
+clearTimeout(timeout);
+});
