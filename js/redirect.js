f4defd6ac0e6c429917c6771ffa083a3ca3b908b
Johan Söderlund <johan.soderlund96@gmail.com>
2023-11-20 01:40:14 +0100

    added redirect to portfolio

--- js/redirect.js
+++ js/redirect.js
@@ -6,6 +6,10 @@ function redirectToCv() {
     window.location.href = "html/cv.html";
 }
 
+function redirectToPortfolio() {
+    window.location.href = "html/portfolio.html";
+}
+
 function redirectToGithub1() {
     window.location.href = "https://github.com/jhn322/pmm-config.git";
 }
