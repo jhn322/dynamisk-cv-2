59d1c32a1c19a4c68e6a40d3a85fa3ecf90b7a71
Johan Söderlund <johan.soderlund96@gmail.com>
2023-12-07 20:31:29 +0100

    changed some repo links

--- js/redirect.js
+++ js/redirect.js
@@ -4,34 +4,42 @@
 // # Denna kod är skriven av mig.#
 // ###############################
 
-
 // function with variabel assigned in HTML to work onclick
 function redirectToAbout() {
-    // Redirects to respective page or external webpage on click
-    window.location.href = "html/about.html";
+  // Redirects to respective page or external webpage on click
+  window.location.href = "html/about.html";
 }
 
 function redirectToCv() {
-    window.location.href = "html/cv.html";
+  window.location.href = "html/cv.html";
 }
 
 function redirectToPortfolio() {
-    window.location.href = "html/portfolio.html";
+  window.location.href = "html/portfolio.html";
 }
 
 function redirectToGithub1() {
-    window.location.href = "https://github.com/jhn322/pmm-config.git";
+  window.location.href = "https://github.com/jhn322/Plex-meta-manager.git";
 }
 
 function redirectToGithub2() {
-    window.location.href = "https://github.com/jhn322/gissa-numret.git";
+  window.location.href = "https://github.com/jhn322/guess-the-number.git";
 }
 
 function redirectToGithub3() {
-    window.location.href = "https://github.com/jhn322/responsive-sidebar-1.git";
+  window.location.href = "https://github.com/jhn322/responsive-sidebar-1.git";
 }
 
 function redirectToGithub4() {
-    window.location.href = "https://github.com/jhn322/docker-linux-server-setup.git";
+  window.location.href =
+    "https://github.com/jhn322/docker-linux-server-setup.git";
 }
-console.log(redirectToAbout, redirectToCv, redirectToPortfolio, redirectToGithub1, redirectToGithub2, redirectToGithub3, redirectToGithub4);
+console.log(
+  redirectToAbout,
+  redirectToCv,
+  redirectToPortfolio,
+  redirectToGithub1,
+  redirectToGithub2,
+  redirectToGithub3,
+  redirectToGithub4
+);
